af7d09e00750891db0dba098f6ebc5fc8b24251a
LeonardGombert <45963857+LeonardGombert@users.noreply.github.com>
2018-12-18 16:59:49 +0100

    Pathfinding
    Have begun coding pathfinding and generated map. Can determine each tile position in world thanks to script.
    
    Tutorial Progress:
    https://www.youtube.com/watch?v=0Q2z5g28ows&index=3&list=PLbghT7MmckI55gwJLrDz0UtNfo9oC0K1Q

---
+++ ZELDALIKE_Movement/Assets/Scripts/ClickableTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableTile : MonoBehaviour {
+
+    public int tileX;
+    public int tileY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Click on " + tileX + " and " + tileY);
+    }
+}
---
+++ ZELDALIKE_Movement/Assets/Scripts/TileMap.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour {
+
+    public TileType [] tileTypes;
+    public GameObject selectedUnit;
+
+    int [,] tiles;
+
+    int mapSizeX = 10;
+    int mapSizeY = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        GenerateMapData();
+        GenerateMapVisual();
+    }
+
+    void GenerateMapData()
+    {
+        //Allocate Map tiles
+        tiles = new int[mapSizeX, mapSizeY];
+
+        //Initialize map floor tiles
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                tiles[x, y] = 0;
+            }
+        }
+        //Let's make a U shaped wall formation
+
+        tiles[4, 4] = 0;
+        tiles[5, 4] = 0;
+        tiles[6, 4] = 0;
+        tiles[7, 4] = 0;
+        tiles[8, 4] = 0;
+
+        tiles[4, 4] = 1;
+        tiles[4, 4] = 1;
+        tiles[8, 4] = 1;
+        tiles[8, 4] = 1;
+	}
+
+    void GenerateMapVisual()
+    {
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                TileType tt = tileTypes[ tiles[x,y] ];
+
+                GameObject gameObject = (GameObject)Instantiate(tt.tileVisualPrefab, new Vector2(x, y), Quaternion.identity);
+
+                //Gets the X and Y location of whichever tile is clicked
+                ClickableTile ct = gameObject.GetComponent<ClickableTile>(); 
+                ct.tileX = x;
+                ct.tileY = y;
+            }
+        }
+    }
+
+    public void MoveSelectedUnitTo (int x, int y)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
